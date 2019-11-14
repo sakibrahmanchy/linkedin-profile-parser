141e960f0a0582ed7117631ee144459532b8a678
Sakibur Rahaman <sakib.cse11.cuet@gmail.com>
2019-11-15 01:59:57 +0600

    Changes

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,7 +1,7 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Redirect, Res } from '@nestjs/common';
 import { AppService } from './app.service';
 import axios from 'axios';
-import cheerio from 'cheerio';
+const cheerio = require('cheerio')
 const puppeteer = require('puppeteer');
 
 @Controller()
@@ -9,19 +9,23 @@ export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  async getHello(): Promise<void> {
-    const browser = await puppeteer.launch();
+  @Redirect('http://localhost:4200/start', 302)
+  async getParsed(@Res() res) {
+    const browser = await puppeteer.launch({ headless: false, userDataDir: './user_data' });
     const page = await browser.newPage();
     await page.goto('https://www.linkedin.com/in/sakibur-rahaman-chowdhury-77359b132/');
-    await page.waitFor(1000);
-    await page.screenshot({ path: 'my-profile-screen-shot.png' });
-    const result = await page.evaluate((node) => {
-      // console.log(node);
-      const name = document.querySelector('.ember-view');
-      // let title = document.querySelector('.headline').innerText;
-      // let summary = document.querySelector('.headline').childNodes[0].innerText;
-
-      console.log(name);
-    });
+    const content = await page.content();
+    await page.close();
+    return content;
+    // await page.goto('https://www.facebook.com/freakinsakib');
+    // // await page.screenshot({ path: 'my-profile-screen-shot.png' });
+    // const result = await page.evaluate((node) => {
+    //   // console.log(node);
+    //   // const name = document.querySelector('.ember-view');
+    //   // let title = document.querySelector('.headline').innerText;
+    //   // let summary = document.querySelector('.headline').childNodes[0].innerText;
+    //
+    //   console.log(name);
+    // });
   }
 }
