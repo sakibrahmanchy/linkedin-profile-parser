c10d0045f12fb026275afb3be0f3ec52c2717a12
sakibrahman <sakib.cse11.cuet@gmail.com>
2019-11-14 20:17:24 +0600

    Cookie Parse

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,8 +1,7 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
-import axios from 'axios';
-import cheerio from 'cheerio';
-const puppeteer = require('puppeteer');
+import * as cheerio from 'cheerio';
+import * as puppeteer from 'puppeteer';
 
 @Controller()
 export class AppController {
@@ -10,18 +9,35 @@ export class AppController {
 
   @Get()
   async getHello(): Promise<void> {
-    const browser = await puppeteer.launch();
+    const browser = await puppeteer.launch({ headless: false, userDataDir: './user_data' });
     const page = await browser.newPage();
-    await page.goto('https://www.linkedin.com/in/sakibur-rahaman-chowdhury-77359b132/');
-    await page.waitFor(1000);
-    await page.screenshot({ path: 'my-profile-screen-shot.png' });
-    const result = await page.evaluate((node) => {
-      // console.log(node);
-      const name = document.querySelector('.ember-view');
-      // let title = document.querySelector('.headline').innerText;
-      // let summary = document.querySelector('.headline').childNodes[0].innerText;
-
-      console.log(name);
+    const profileUrl = 'https://www.linkedin.com/in/sakibur-rahaman-chowdhury-77359b132/';
+    const loginUrl = 'https://www.linkedin.com/login';
+    const notPreviouslyLoggedIn = false;
+    await page.goto(profileUrl);
+    const cookiesObject = await page.cookies();
+    const pathCookie = cookiesObject.filter(cookie => {
+      return cookie.name === 'fid';
     });
+    if (pathCookie.path === '/') {
+      await page.goto(loginUrl);
+    } else {
+      const $ = cheerio.load(await page.content());
+      console.log($('.top-card-layout__title').text());
+    }
+    // await page.close();
+    // await browser.close();
+    // const $ = cheerio.load(await page.content());
+
+    // await page.waitFor(1000);
+    // await page.screenshot({ path: 'my-profile-screen-shot.png' });
+    // const result = await page.evaluate((node) => {
+    //   // console.log(node);
+    //   const name = document.querySelector('.ember-view');
+    //   // let title = document.querySelector('.headline').innerText;
+    //   // let summary = document.querySelector('.headline').childNodes[0].innerText;
+    //
+    //   console.log(name);
+    // });
   }
 }
