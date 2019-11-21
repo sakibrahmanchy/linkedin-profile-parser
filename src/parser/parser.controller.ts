9c0858478c018f0e1405f06461a5f7b4961637aa
Sakibur Rahaman <sakib.cse11.cuet@gmail.com>
2019-11-21 23:46:07 +0600

    Headless

--- src/parser/parser.controller.ts
+++ src/parser/parser.controller.ts
@@ -8,24 +8,11 @@ export class ParserController {
   constructor(private readonly parserService: ParserService) {}
 
   @Get('/parse')
-  // @Redirect('http://192.168.0.102:4200/start', 302)
   async getParsed(@Req() req, @Res() res) {
       const linkedinUrl = req.query.url ? req.query.url : 'https://www.linkedin.com/in/sakibur-rahaman-chowdhury-77359b132/';
       const data = await this.parserService.parse(linkedinUrl);
+      console.log(data);
+      res.redirect('http://192.168.0.102:4200/');
       console.log("Data has been parsed");
     }
-    // await page.close();
-    // await browser.close();
-    // const $ = cheerio.load(await page.content());
-
-    // await page.waitFor(1000);
-    // await page.screenshot({ path: 'my-profile-screen-shot.png' });
-    // const result = await page.evaluate((node) => {
-    //   // console.log(node);
-    //   const name = document.querySelector('.ember-view');
-    //   // let title = document.querySelector('.headline').innerText;
-    //   // let summary = document.querySelector('.headline').childNodes[0].innerText;
-    //
-    //   console.log(name);
-    // });
 }
--- src/parser/parser.service.ts
+++ src/parser/parser.service.ts
@@ -4,101 +4,82 @@ import * as puppeteer from 'puppeteer';
 import { Experience } from '../interfaces/experience.interface';
 import { Education } from '../interfaces/education.interface';
 import { Project } from '../interfaces/project.interface';
-import * as fs from 'fs';
-
-const { Cluster } = require('puppeteer-cluster');
 
 @Injectable()
 export class ParserService {
-  async parse(linkedInUrl: string) {
-    const cluster = await Cluster.launch({
-      concurrency: Cluster.CONCURRENCY_CONTEXT,
-      maxConcurrency: 10,
-      puppeteerOptions: {
-        headless: false,
-        userDataDir: './user_data',
-      },
+  async parse(url: string) {
+    const browser = await puppeteer.launch({ headless: true, userDataDir: './user_data', args: ['--no-sandbox', '--disable-setuid-sandbox']});
+    const page = await browser.newPage();
+    const profileUrl = url;
+    const loginUrl = 'https://www.linkedin.com/login';
+    const notPreviouslyLoggedIn = false;
+    await page.goto(profileUrl);
+    await page.screenshot({ path: 'screenshot.jpg'});
+    await page.setUserAgent
+    ('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36');
+
+    const cookiesObject = await page.cookies();
+    const pathCookie = cookiesObject.filter(cookie => {
+      return cookie.name === 'fid';
     });
-
-    await cluster.task(async ({ page, data: url }) => {
-      // const browser = await puppeteer.launch({ headless: false, userDataDir: './user_data' });
-      // const page = await browser.newPage();
-      const profileUrl = url;
-      const loginUrl = 'https://www.linkedin.com/login';
-      const notPreviouslyLoggedIn = false;
-      await page.goto(profileUrl);
-      const cookiesObject = await page.cookies();
-      const pathCookie = cookiesObject.filter(cookie => {
-        return cookie.name === 'fid';
-      });
-      if (pathCookie.path === '/') {
-        await page.goto(loginUrl);
-      } else {
-        const $ = cheerio.load(await page.content());
-        const experiences: Experience[] = [];
-        $('.experience li').each((ex, item) => {
-          const experience: Experience = {
-            title: $(item).find('.experience-item__title').text(),
-            subTitle: $(item).find('.experience-item__subtitle-link').text(),
-            startDate: $(item).find('.date-range__start-date').text(),
-            endDate: $(item).find('.date-range__end-date').text(),
-            duration: $(item).find('.date-range__duration').text(),
-            location: $(item).find('.experience-item__location').text(),
-            description: $(item).find('.show-more-less-text__text--less').text(),
-            imageUrl: $(item).find('img').attr('data-delayed-url'),
-          };
-          experiences.push(experience);
-        });
-
-        const educations: Education[] = [];
-        $('.education li').each((ex, item) => {
-          const education: Education = {
-            title: $(item).find('.result-card__title').text(),
-            subTitle: $(item).find('.result-card__subtitle-link').text(),
-            startDate: $(item).find('.date-range__start-date').text(),
-            endDate: $(item).find('.date-range__end-date').text(),
-            duration: $(item).find('.date-range__duration').text(),
-            location: $(item).find('.experience-item__location').text(),
-            activities: $(item).find('.education__item--activities-and-societies').text(),
-            description: $(item).find('.show-more-less-text__text--less').text(),
-            imageUrl: $(item).find('img').attr('data-delayed-url'),
-          };
-          educations.push(education);
-        });
-
-        const projects: Project[] = [];
-        $('.projects li').each((ex, item) => {
-          const project: Project = {
-            title: $(item).find('.result-card__title-link').text(),
-            startDate: $(item).find('.date-range__start-date').text(),
-            endDate: $(item).find('.date-range__end-date').text(),
-            duration: $(item).find('.date-range__duration').text(),
-            description: $(item).find('.show-more-less-text__text--less').text(),
-            link: $(item).find('.personal-project__button').attr('href'),
-          };
-          projects.push(project);
-        });
-        const data = {
-          name:  $('.top-card-layout__title').text(),
-          headline: $('.top-card-layout__headline').text(),
-          about: $('.summary > p').text(),
-          experiences,
-          educations,
-          projects,
+    if (pathCookie.path === '/') {
+      await page.goto(loginUrl);
+    } else {
+      const $ = cheerio.load(await page.content());
+      const experiences: Experience[] = [];
+      $('.experience li').each((ex, item) => {
+        const experience: Experience = {
+          title: $(item).find('.experience-item__title').text(),
+          subTitle: $(item).find('.experience-item__subtitle-link').text(),
+          startDate: $(item).find('.date-range__start-date').text(),
+          endDate: $(item).find('.date-range__end-date').text(),
+          duration: $(item).find('.date-range__duration').text(),
+          location: $(item).find('.experience-item__location').text(),
+          description: $(item).find('.show-more-less-text__text--less').text(),
+          imageUrl: $(item).find('img').attr('data-delayed-url'),
         };
-        await page.close();
-        await fs.writeFile("user-data-file.txt", data, function(err) {
+        experiences.push(experience);
+      });
 
-          if(err) {
-            return console.log(err);
-          }
+      const educations: Education[] = [];
+      $('.education li').each((ex, item) => {
+        const education: Education = {
+          title: $(item).find('.result-card__title').text(),
+          subTitle: $(item).find('.result-card__subtitle-link').text(),
+          startDate: $(item).find('.date-range__start-date').text(),
+          endDate: $(item).find('.date-range__end-date').text(),
+          duration: $(item).find('.date-range__duration').text(),
+          location: $(item).find('.experience-item__location').text(),
+          activities: $(item).find('.education__item--activities-and-societies').text(),
+          description: $(item).find('.show-more-less-text__text--less').text(),
+          imageUrl: $(item).find('img').attr('data-delayed-url'),
+        };
+        educations.push(education);
+      });
 
-          console.log("The file was saved!");
-        });
-        return data;
-      }
-
-      cluster.queue(linkedInUrl);
-    });
+      const projects: Project[] = [];
+      $('.projects li').each((ex, item) => {
+        const project: Project = {
+          title: $(item).find('.result-card__title-link').text(),
+          startDate: $(item).find('.date-range__start-date').text(),
+          endDate: $(item).find('.date-range__end-date').text(),
+          duration: $(item).find('.date-range__duration').text(),
+          description: $(item).find('.show-more-less-text__text--less').text(),
+          link: $(item).find('.personal-project__button').attr('href'),
+        };
+        projects.push(project);
+      });
+      const data = {
+        name:  $('.top-card-layout__title').text(),
+        headline: $('.top-card-layout__headline').text(),
+        about: $('.summary > p').text(),
+        experiences,
+        educations,
+        projects,
+      };
+      await page.close();
+      await browser.close();
+      return data;
+    }
   }
 }
